06566e9733976a110a1aeaaaa1b50cc621ae9dce
Rahul <krahul180300@gmail.com>
2025-11-16 17:24:54 +0530

    render

--- public/script.js
+++ public/script.js
@@ -20,7 +20,7 @@ async function ask() {
     questionInput.value = "";
 
     try {
-        const res = await fetch("/ask", {
+        const res = await fetch("https://team-activity-monitor.onrender.com/ask", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ question })
